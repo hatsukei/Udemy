55a0138ac527619f45348fbba246cc3efd116d64
root <root@DESKTOP-RH8HL0V.localdomain>
2021-01-18 02:46:37 -0500

    adding to section 4

---
+++ components/src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          commentText="Yo yo yo"
+          avatarImg={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          commentText="Nice"
+          avatarImg={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          commentText="Wow"
+          avatarImg={faker.image.image()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,15 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 // Create a react component
 const App = () => {
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
   return (
     <div>
-      <label class="label" for="name">
-        Enter Name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
-      <button style="background-color: blue; color: white;">Submit</button>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
     </div>
   );
 };
